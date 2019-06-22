169310c9f48edc9787f2de42f7e2f90abe7cf25b
Manson36 <469211209@qq.com>
2019-06-22 22:23:34 +0800

    [Test]Programtesting

---
+++ container/2/nonRepeating2_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestSubstr(t *testing.T) {
+	tests := []struct{
+		s string
+		ans int
+	}{
+		//normal cases
+		{"abcabcbb", 3},
+		{"pwwkew", 3},
+
+		//Edge cases
+		{"", 0},
+		{"b", 1},
+		{"bbbbbb", 1},
+		{"abcabcabcd", 4},
+
+		//chinese
+		{"这里啥也不是", 6},
+		{"一二三二一", 3},
+		{"黑化肥挥发会发灰会花飞灰化肥挥发发黑会飞花",8},
+	}
+
+	for _, tt := range tests {
+		actual := lengthOfNonRepeatingSunStr2(tt.s)
+		if actual != tt.ans {
+			t.Errorf("got %d for input %s; expected %d", actual, tt.s, tt.ans)
+		}
+	}
+}
--- errhaddling/filelistingserver/web.go
+++ errhaddling/filelistingserver/web.go
@@ -11,7 +11,16 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(writer http.ResponseWriter, request *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+			log.Printf("Panic: %v", r)
+			http.Error(writer, http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError)
+			}
+		}()
+
 		err := handler(writer, request)
+
 		if err != nil {
 			log.Printf("Error handling request: %s", err)
 
